f3980704b19d4f6acc98c7b0c1fc342f8a00692d
macgill <macgill4444@gmail.com>
2014-09-12 09:49:33 -0400

    adding all worked on files

---
+++ home.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){ 
+
+	$(".my_name").mouseover(function(){
+		$(this).css("color","royalblue");
+		$(this).css("text-shadow","-1px 0 red, 0 1px red, 1px 0 red, 0 -1px red")
+	});
+
+	$(".my_name").mouseleave(function(){
+		$(this).css("color","#333");
+		$(this).css("text-shadow","none")
+
+	});
+
+	function skrollr_init() {
+		var x = skrollr.init({
+			render: function(data) {
+				//Debugging - Log the current scroll position.
+            	console.log(data.curTop);
+			}
+		})
+	}
+	skrollr_init();
+
+
+
+
+
+})
